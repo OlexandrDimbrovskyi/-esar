bd6b629dbcc55ef0b78bb6ef347a39146b1f6ec8
OlexandrDimbrovskyi <55288105+OlexandrDimbrovskyi@users.noreply.github.com>
2019-11-02 10:47:11 +0200

    Add files via upload

---
+++ Шифр Цезаря.py
@@ -0,0 +1,18 @@
+
+i = 5
+
+while True:
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUYWXYZabcdefghijklmopqrstuvwxyzАБВГДЕЄЖЗИІЇйКЛМНОПРСТУФЧЦЧШЩЬЮЯабвгдеєжзиіїйклмнопрстуфхцшщбюяАБВГДЕЄЖЗИІЇйКЛМНОПРСТУФЧЦЧШЩЬЮЯабвгдеєжзиіїйклмнопрстуфхцшщбюя01234567890123456789"
+    encrypt = input("Введіть ваше речення: ")
+    key = int(input("Будь-ласка введіть ключ (цифру 1-25): "))
+    encrypted = ""
+    if encrypt == "exit":
+        break
+    for letter in encrypt:
+        position = alphabet.find(letter)
+        newPosition = position + key
+        if letter in alphabet:
+            encrypted = encrypted + alphabet[newPosition]
+        else:
+            encrypted = encrypted + letter
+    print("Ваше зашифроване речення: ", encrypted)
